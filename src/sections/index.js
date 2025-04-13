0b822be8d7e1e5d441dca5ab2ab82ae620f621c3
Stefan Nikolic <stefan.nikolic@smail.th-koeln.de>
2025-04-13 12:55:13 +0200

    All Sections created - responsive padding for diffrent devices

---
+++ src/sections/index.js
@@ -0,0 +1,21 @@
+import CustomerReviews from './CustomerReviews';
+import Nav from './Nav';
+import Footer from './Footer';
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import SuperQuality from './CustomerReviews';
+import Subscribe from './Subcribe'
+
+export{
+  CustomerReviews,
+  Nav,
+  Footer,
+  Hero,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality
+}
