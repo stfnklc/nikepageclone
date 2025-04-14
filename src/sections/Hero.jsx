217e8799d38af4e255c2e306eab4795c9f58ff15
Stefan Nikolic <stefan.nikolic@smail.th-koeln.de>
2025-04-14 12:19:17 +0200

    Fixed responsiveness of Heropage for smaller Devices

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
   return (
     <section
     id="home"
-    className="w-full flex xl:flex-rol justify-center min-h-screen gap-10 max-container"
+    className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
     >
       <div className="ralative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
         <p className="text-xl font-montserrat text-coral-red">Our Summer Collection</p>
