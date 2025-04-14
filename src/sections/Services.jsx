cd78e7a79f05199acbcd51a1817e0419374e8376
Stefan Nikolic <stefan.nikolic@smail.th-koeln.de>
2025-04-14 14:55:57 +0200

    Super Quality and Services sections

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,6 +1,12 @@
+import { services } from "../constants"
+import ServicesCard from "../components/ServicesCard"
 const Services = () => {
   return (
-    <div>Services</div>
+    <section className="max-container flex justify-center flex-wrap gap-9">
+      {services.map((service)=>(
+        <ServicesCard key={service.label}{...service} />
+      ))}
+    </section>
   )
 }
 
--- src/sections/index.js
+++ src/sections/index.js
@@ -4,7 +4,7 @@ import Hero from './Hero';
 import PopularProducts from './PopularProducts';
 import Services from './Services';
 import SpecialOffer from './SpecialOffer';
-import SuperQuality from './CustomerReviews';
+import SuperQuality from './SuperQuality';
 import Subscribe from './Subcribe'
 
 export{
